fffb5b98090741134ef125954f0264fea368b9ba
The30Note <The30Note@gmail.com>
2023-07-17 21:01:52 -0500

    First basic checks commit

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,39 @@
-use clap::{App, Arg};
+use log::{debug, error, info, Level, Metadata};
+use std::path::Path;
 
 fn main() {
-    let app = App::new("my-rust-program")
-        .version("1.0.0")
-        .author("Bard")
-        .about("A simple Rust program that takes in flags")
-        .arg(
-            Arg::new("file")
-                .short("f")
-                .long("file")
-                .help("The file to read")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::new("number")
-                .short("n")
-                .long("number")
-                .help("A number to print")
-                .takes_value(true),
-        );
 
-    let args = app.parse_args();
+    // Init Logger
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("trace")).init();
 
-    let file = args.value_of("file").unwrap_or("");
-    let number = args.value_of("number").unwrap_or("");
+    // Check if running as root
+    if std::env::var("SUDO_USER").is_ok() == false { error!("This script requires root privileges!"); }
 
-    println!("File: {}", file);
-    println!("Number: {}", number);
+    // Get cpuinfo
+    let cpu_info = procfs::CpuInfo::new().unwrap();
+    let cpu_model_name = cpu_info.model_name(0).unwrap();
+    let cpu_flags = cpu_info.flags(0).unwrap();
+
+    // svm / vmx check
+    if cpu_flags.contains(&"svm") {
+        info!("CPU supports svm");
+    } else if cpu_flags.contains(&"vmx") {
+        info!("CPU supports vmx");
+    } else {
+        error!("This system doesn't support virtualization, please enable it then run this script again!")
+    }
+    
+    // Check if system is installed in UEFI mode
+    if Path::new("/sys/firmware/efi").exists() {
+        info!("System installed in UEFI mode");
+    } else {
+        error!("This system isn't installed in UEFI mode!");
+    }
+
+    // IOMMU check
+    if Path::new("/sys/class/iommu/").read_dir().unwrap().any(|entry| entry.is_ok()) {
+        info!("IOMMU is enabled");
+    } else {
+        error!("This system doesn't support IOMMU, please enable it then run this script again!");
+    }
 }
